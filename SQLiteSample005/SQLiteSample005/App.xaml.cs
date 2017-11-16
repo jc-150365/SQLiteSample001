c8f94010a723b5a6fe9ee589583c9bd0c8c076d9
謙太 可士村 <150365@jc-21.jp>
2017-11-16 11:13:30 +0900

    App.xaml.csに partial付けた

--- SQLiteSample005/SQLiteSample005/App.xaml.cs
+++ SQLiteSample005/SQLiteSample005/App.xaml.cs
@@ -3,7 +3,7 @@ using Xamarin.Forms;
 
 namespace SQLiteSample005
 {
-    public class App : Application
+    public partial class App : Application //partial付けた
     {
         public App()
         {
@@ -14,19 +14,19 @@ namespace SQLiteSample005
     class MyPage : ContentPage
     {
 
-        readonly TodoRepository _db = new TodoRepository(); // <-1
+        readonly TodoRepository _db = new TodoRepository(); 
 
         public MyPage()
         {
 
             var listView = new ListView
-            { // <-2
-                ItemsSource = _db.GetItems(), // <-3
-                ItemTemplate = new DataTemplate(typeof(TextCell)) // <-4
+            { 
+                ItemsSource = _db.GetItems(), 
+                ItemTemplate = new DataTemplate(typeof(TextCell)) 
             };
             listView.ItemTemplate.SetBinding(TextCell.TextProperty, "Text");
             listView.ItemTemplate.SetBinding(TextCell.DetailProperty, new Binding("CreatedAt", stringFormat: "{0:yyy/MM/dd hh:mm}"));
-            listView.ItemTapped += async (s, a) => { // <-5
+            listView.ItemTapped += async (s, a) => { 
                 var item = (TodoItem)a.Item;
                 if (await DisplayAlert("削除してい宜しいですか", item.Text, "OK", "キャンセル"))
                 {
@@ -36,16 +36,16 @@ namespace SQLiteSample005
                 }
             };
             var entry = new Entry
-            { // <-6
+            { 
                 HorizontalOptions = LayoutOptions.FillAndExpand
             };
             var buttonAdd = new Button
-            { // <-7
+            { 
                 WidthRequest = 60,
                 TextColor = Color.White,
                 Text = "Add"
             };
-            buttonAdd.Clicked += (s, a) => { // <-8
+            buttonAdd.Clicked += (s, a) => { 
                 if (!String.IsNullOrEmpty(entry.Text))
                 { // Entryに文字列が入力されている場合に処理する
                     var item = new TodoItem { Text = entry.Text, CreatedAt = DateTime.Now, Delete = false };
@@ -56,7 +56,7 @@ namespace SQLiteSample005
             };
 
             Content = new StackLayout
-            { // <-9
+            { 
                 Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0),
                 Children = {
           new StackLayout {
